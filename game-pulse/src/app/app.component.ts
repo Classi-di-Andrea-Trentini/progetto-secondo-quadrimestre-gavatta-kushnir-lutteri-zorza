28225f74f471156d05ad4d717a7919abfe13ed08
gabriellutteri <144794600+gabriellutteri@users.noreply.github.com>
2025-01-30 11:06:35 +0000

    base-ai

--- game-pulse/src/app/app.component.ts
+++ game-pulse/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OpenaiService } from './services/openai.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'game-pulse';
+  openaiService: OpenaiService = inject(OpenaiService);
+
+  testAI() {
+    this.openaiService.ask('Questo è un test di chatgpt').subscribe(result => {
+      console.log(result);
+    })
+  }
 }
