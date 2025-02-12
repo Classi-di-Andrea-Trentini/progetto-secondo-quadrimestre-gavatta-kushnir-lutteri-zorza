e211ddce704a87193eb23d525c15b77dcc4c59a3
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-02-12 13:32:43 +0000

    centrato mostra password

--- game-pulse/src/app/login/login.component.ts
+++ game-pulse/src/app/login/login.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true, // Aggiungi questa riga se stai usando Angular standalone components
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
   showPassword: boolean = false;
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log('Username:', this.username);
+      console.log('Password:', this.password);
+    }
+  }
+
+  annulla(): void {
+    //tornare alla pagina iniziale
+    console.log('Annulla');
+  }
+
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
