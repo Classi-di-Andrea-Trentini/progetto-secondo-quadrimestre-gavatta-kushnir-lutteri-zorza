9f27f1f252abf1bcb560a37e26317b07ffdbb0e5
Oleksandr-Kushnir <114574060+oleksandrkushnir1@users.noreply.github.com>
2025-02-12 13:36:03 +0000

    _

---
+++ game-pulse/src/app/game-page/game-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-game-page',
+  imports: [],
+  templateUrl: './game-page.component.html',
+  styleUrl: './game-page.component.css'
+})
+export class GamePageComponent {
+  
+}
