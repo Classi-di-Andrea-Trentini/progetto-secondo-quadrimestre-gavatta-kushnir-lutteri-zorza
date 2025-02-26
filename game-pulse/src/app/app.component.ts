b9e03b60e3476d71ac301438dd1d54e9147c324f
Oleksandr-Kushnir <114574060+oleksandrkushnir1@users.noreply.github.com>
2025-02-26 13:34:42 +0000

    _

--- game-pulse/src/app/app.component.ts
+++ game-pulse/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { MenuComponent } from './menu/menu.component';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, LoginComponent, MatSlideToggleModule,RouterOutlet],
+  imports: [MenuComponent, LoginComponent, MatSlideToggleModule,RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- game-pulse/src/app/app.routes.ts
+++ game-pulse/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PageGameComponent } from './page-game/page-game.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
-    { path: 'game/:id', component: PageGameComponent}
+    { path: '', component: HomeComponent },
+    { path: 'game/:id', component: PageGameComponent},
+    { path: 'login', component: LoginComponent}
     
 ];
