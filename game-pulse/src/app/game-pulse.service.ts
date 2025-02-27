eead6dcfc854c3c943adc0326d233bc03f540d89
Oleksandr-Kushnir <114574060+oleksandrkushnir1@users.noreply.github.com>
2025-02-27 11:02:05 +0000

    _

--- game-pulse/src/app/game-pulse.service.ts
+++ game-pulse/src/app/game-pulse.service.ts
@@ -7,7 +7,7 @@ import { InfoGioco } from './info-gioco';
   providedIn: 'root'
 })
 export class GamePulseService {
-
+  private urlprice: string = "https://www.cheapshark.com/redirect?dealID=";
   private url: string = "https://api.rawg.io/api/games"
   private key: string = "?key=9abd9c1ccd8c48a88b995b17c20b2d94" // e' una key che si richiede una volta solo quindi lo gia messo nel url
   constructor() { }
@@ -21,6 +21,13 @@ export class GamePulseService {
   searchGameById(id: String): Observable<InfoGioco> {
     const urlcompleted = `${this.url}/${id}${this.key}`;
     return this.httpClient.get<InfoGioco>(urlcompleted);
+  }
+
+
+  requestDealPrice(gameName: string): Observable<any> {
+    const url = "https://api.rawg.io/api/games";
+    const params = new HttpParams().set('key', '9abd9c1ccd8c48a88b995b17c20b2d94').set('search', gameName);
+    return this.httpClient.get(url, { params });
   }
 
 
