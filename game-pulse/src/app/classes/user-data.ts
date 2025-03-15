c9842caaff4a626b4a9dd3f16150a42f57f91d03
Oleksandr-Kushnir <114574060+oleksandrkushnir1@users.noreply.github.com>
2025-03-15 11:11:13 +0000

    sistemazione user account ma e' ancora da lavorarci su per adesso finisco la logica

--- game-pulse/src/app/classes/user-data.ts
+++ game-pulse/src/app/classes/user-data.ts
@@ -1,17 +1,18 @@
 import { UserMetadata, UserInfo, IdTokenResult } from "firebase/auth";
 import { IUserData } from "../interfaces/i-user-data";
 import { InfoGioco } from "./info-gioco";
+import { GiocoVenduto } from "./gioco-venduto";
 
 export class UserData {
     uid: string;
-    games: InfoGioco[]
+    giochi_in_vendita: GiocoVenduto[]
     displayName: null | string;
     email: null | string;
     photoURL: null | string;
 
     constructor(uid: string, displayName: null | string,email: null | string, photoURL: null | string) {       
         this.uid = uid;
-        this.games = [];
+        this.giochi_in_vendita = [];
         this.displayName = displayName;
         this.email = email;
         this.photoURL = photoURL;
