50d0be8ad7bda089668f5bc05845903567ea2093
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-03-20 20:18:09 +0000

    impostati descrizione e titolo

--- game-pulse/src/app/components/page-game/descrizione/descrizione.component.ts
+++ game-pulse/src/app/components/page-game/descrizione/descrizione.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InfoGioco } from '../../../classes/info-gioco';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-descrizione',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './descrizione.component.html',
   styleUrl: './descrizione.component.css'
 })
 export class DescrizioneComponent {
-
+  @Input() infoGioco: InfoGioco | null = null;
 }
