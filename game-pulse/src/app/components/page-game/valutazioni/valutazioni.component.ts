1625675204a96e19d5cf5a1d23e40269698623c5
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-10 09:44:22 +0000

    introdotto API per le valutazioni

--- game-pulse/src/app/components/page-game/valutazioni/valutazioni.component.ts
+++ game-pulse/src/app/components/page-game/valutazioni/valutazioni.component.ts
@@ -1,6 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { InfoGioco } from '../../../classes/info-gioco';
 import { CommonModule } from '@angular/common';
+
+interface Rating {
+  id: number;
+  title: string;
+  count: number;
+  percent: number;
+}
 
 @Component({
   selector: 'app-valutazioni',
@@ -8,12 +15,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './valutazioni.component.html',
   styleUrl: './valutazioni.component.css'
 })
-export class ValutazioniComponent {
+export class ValutazioniComponent implements OnInit {
   @Input() infoGioco: InfoGioco | null = null;
 
-  get valutazioni(): string[] {
-    return this.infoGioco?.ratings?.map(rating => rating.title) || [];
+  get valutazioni(): Rating[] {
+    return this.infoGioco?.ratings || [];
   }
   
-
+  // Funzione per determinare il colore del badge in base al titolo della valutazione
+  getRatingColor(title: string): string {
+    switch(title.toLowerCase()) {
+      case 'exceptional': return 'exceptional';
+      case 'recommended': return 'recommended';
+      case 'meh': return 'meh';
+      case 'skip': return 'skip';
+      default: return 'default';
+    }
+  }
+  
+  ngOnInit(): void {
+    console.log("valutazioni");
+    console.log(this.valutazioni);
+  }
 }
