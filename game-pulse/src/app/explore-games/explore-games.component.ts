bc46be5de7eeb6d201b262b42e42e81ba6d5e35a
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-18 18:35:09 +0000

    Aggiunta barra laterale per la selezione dei generi e migliorata la disposizione dei giochi nella griglia

--- game-pulse/src/app/explore-games/explore-games.component.ts
+++ game-pulse/src/app/explore-games/explore-games.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BarraLateraleGeneriComponent } from './barra-laterale-generi/barra-laterale-generi.component';
+
 
 @Component({
   selector: 'app-explore-games',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BarraLateraleGeneriComponent],
   templateUrl: './explore-games.component.html',
   styleUrl: './explore-games.component.css'
 })
