ea8e99ec1f28dc179431c320dfc235e437b54851
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-18 18:54:03 +0000

    feat: implemetato game-card component

--- game-pulse/src/app/explore-games/explore-games.component.ts
+++ game-pulse/src/app/explore-games/explore-games.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarraLateraleGeneriComponent } from './barra-laterale-generi/barra-laterale-generi.component';
-
+import { GameCardComponent } from './game-card/game-card.component';
 
 @Component({
   selector: 'app-explore-games',
   standalone: true,
-  imports: [CommonModule, BarraLateraleGeneriComponent],
+  imports: [CommonModule, BarraLateraleGeneriComponent, GameCardComponent],
   templateUrl: './explore-games.component.html',
   styleUrl: './explore-games.component.css'
 })
 export class ExploreGamesComponent {
-
+  games = Array(15).fill(0).map((_, i) => ({
+    id: i + 1,
+    title: `Titolo del Gioco ${i + 1}`,
+    genre: 'Azione, Avventura',
+    rating: '★★★★☆',
+    price: '€59.99'
+  }));
 }
