065eff7bbe12aee7e4196ad20cb43cb1b77c3021
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-19 08:30:35 +0000

    feat: migliorata la disposizione e il caricamento dei giochi nella pagina esplora, introdotto inoltre iil servizio per ottenere i migliori giochi valutati

--- game-pulse/src/app/components/explore-games/explore-games.component.ts
+++ game-pulse/src/app/components/explore-games/explore-games.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarraLateraleGeneriComponent } from './barra-laterale-generi/barra-laterale-generi.component';
 import { GameCardComponent } from './game-card/game-card.component';
+import { GamePulseService } from '../../services/game-pulse.service';
 
 @Component({
   selector: 'app-explore-games',
@@ -10,12 +11,37 @@ import { GameCardComponent } from './game-card/game-card.component';
   templateUrl: './explore-games.component.html',
   styleUrl: './explore-games.component.css'
 })
-export class ExploreGamesComponent {
-  games = Array(15).fill(0).map((_, i) => ({
+export class ExploreGamesComponent{
+/*   games = Array(15).fill(0).map((_, i) => ({
     id: i + 1,
     title: `Titolo del Gioco ${i + 1}`,
     genre: 'Azione, Avventura',
     rating: '★★★★☆',
     price: '€59.99'
-  }));
+  })); */
+
+  games: any[] = [];
+  isLoading = true;
+  error: string | null = null;
+
+  constructor(private GamePulseService: GamePulseService) {}
+
+  ngOnInit() {
+    this.GamePulseService.getTopRatedGames().subscribe({
+      next: (response: any) => {
+        this.games = response.results;
+        this.isLoading = false;
+        this.games.forEach((game: any) => {
+          game.rating = '★★★★★'; // Placeholder for rating
+          game.price = '€59.99'; // Placeholder for price
+          game.genre = game.genres.map((genre: any) => genre.name).join(', '); // Join genres into a string
+        });
+      },
+      error: (err) => {
+        this.error = 'Failed to load games';
+        this.isLoading = false;
+      }
+    });
+    
+  }
 }
