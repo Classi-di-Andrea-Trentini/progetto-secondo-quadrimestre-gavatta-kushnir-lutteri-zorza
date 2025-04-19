d036179c83c2a6edf75c56681a2e69224ed68f23
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-19 09:06:26 +0000

    feat: introduzione dell'api "IsThereAnyDeal" per i prezzi dei vasri giochi

---
+++ game-pulse/src/app/services/itad.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment2 } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItadService {
+  private readonly apiUrl = environment2.ITAD_API_URL;
+  private readonly apiKey = environment2.ITAD_API_KEY;
+
+  constructor(private http: HttpClient) {}
+
+  // Esempio: Ottieni i prezzi per un gioco
+  getGamePrices(plain: string, region: string = 'us') {
+    const params = new HttpParams()
+      .set('key', this.apiKey)
+      .set('plains', plain)
+      .set('region', region)
+      .set('country', 'US');
+
+    return this.http.get(`${this.apiUrl}/game/prices/`, { params });
+  }
+}
