5688c5d6a79e2757e6589f955f36090c7d735361
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-27 11:47:17 +0000

    aggiunto nuovo componente

---
+++ game-pulse/src/app/components/new-homepage/new-homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-homepage',
+  imports: [],
+  templateUrl: './new-homepage.component.html',
+  styleUrl: './new-homepage.component.css'
+})
+export class NewHomepageComponent {
+
+}
