a8ac0c01fac7b73eb28562970ce7922f91de9ac1
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-04-27 12:45:56 +0000

    style:impostato parallelogramma

--- game-pulse/src/app/components/new-homepage/new-homepage.component.ts
+++ game-pulse/src/app/components/new-homepage/new-homepage.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-homepage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './new-homepage.component.html',
   styleUrl: './new-homepage.component.css'
 })
