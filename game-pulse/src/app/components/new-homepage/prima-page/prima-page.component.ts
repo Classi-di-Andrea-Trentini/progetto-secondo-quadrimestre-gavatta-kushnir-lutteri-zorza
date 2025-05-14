e3daff30a2f507db21a6e1b4fc88e4ebea7f6a78
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-05-14 12:15:40 +0000

    nuova hero section

---
+++ game-pulse/src/app/components/new-homepage/prima-page/prima-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prima-page',
+  imports: [],
+  templateUrl: './prima-page.component.html',
+  styleUrl: './prima-page.component.css'
+})
+export class PrimaPageComponent {
+
+}
---
+++ game-pulse/src/app/components/new-homepage/seconda-page/seconda-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seconda-page',
+  imports: [],
+  templateUrl: './seconda-page.component.html',
+  styleUrl: './seconda-page.component.css'
+})
+export class SecondaPageComponent {
+
+}
