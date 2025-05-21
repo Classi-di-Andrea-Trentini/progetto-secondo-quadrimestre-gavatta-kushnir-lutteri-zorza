ea1f3e2c00ad127fa882fc4858c21eb7a8ea0fa8
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-05-21 11:49:19 +0000

    fix: correzione link

--- game-pulse/src/app/components/page-game/buy/prezzi/prezzi.component.ts
+++ game-pulse/src/app/components/page-game/buy/prezzi/prezzi.component.ts
@@ -5,16 +5,15 @@ import { CartButtonComponent } from './cart-button/cart-button.component';
 @Component({
   selector: 'app-prezzi',
   standalone: true,
-  imports: [CommonModule, CartButtonComponent], // Aggiungi CartButtonComponent qui
+  imports: [CommonModule, CartButtonComponent], 
   templateUrl: './prezzi.component.html',
   styleUrls: ['./prezzi.component.css']
 })
 export class PrezziComponent {
   stores = [
-    { name: 'Steam', price: '€59.99', storeUrl: '#' },
-    { name: 'Epic Games', price: '€54.99', storeUrl: '#' },
-    { name: 'GoG', price: '€49.99', storeUrl: '#' }
-    // Aggiungi altri negozi se necessario
+    { name: 'Steam', price: '€59.99', storeUrl: 'https://store.steampowered.com/' },
+    { name: 'Epic Games', price: '€54.99', storeUrl: 'https://www.epicgames.com/site/it/home' },
+    { name: 'GoG', price: '€49.99', storeUrl: 'https://www.gog.com/en/' }
+
   ];
 }
-
