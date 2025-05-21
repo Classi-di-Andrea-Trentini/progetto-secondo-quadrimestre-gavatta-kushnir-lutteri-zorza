0ecc976dc3e66c6350228fc6b20c8c0e11f42287
Jacopo Zorza <144794082+jacopo-zorza@users.noreply.github.com>
2025-05-21 11:34:46 +0000

    fix: sistemazione problematiche con collegamenti nei siti per comprare i giochi

--- game-pulse/src/app/components/page-game/buy/prezzi/prezzi.component.ts
+++ game-pulse/src/app/components/page-game/buy/prezzi/prezzi.component.ts
@@ -11,9 +11,9 @@ import { CartButtonComponent } from './cart-button/cart-button.component';
 })
 export class PrezziComponent {
   stores = [
-    { name: 'Steam', price: '€59.99', storeUrl: '#' },
-    { name: 'Epic Games', price: '€54.99', storeUrl: '#' },
-    { name: 'GoG', price: '€49.99', storeUrl: '#' }
+    { name: 'Steam', price: '€59.99', storeUrl: 'https://store.steampowered.com/' },
+    { name: 'Epic Games', price: '€54.99', storeUrl: 'https://www.epicgames.com/site/it/home' },
+    { name: 'GoG', price: '€49.99', storeUrl: 'https://www.gog.com/en/' }
     // Aggiungi altri negozi se necessario
   ];
 }
