45686f74d0b3b70bb3a3ee1c3b2df2a1467d6eb6
Jacopo Zorza <144794082+jacopo-zorza@users.noreply.github.com>
2025-05-22 09:18:17 +0000

    feat: aggiungi tabella carrello con gestione dinamica degli articoli

--- game-pulse/src/app/components/carello/carello.component.ts
+++ game-pulse/src/app/components/carello/carello.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../services/auth.service';
 import { UserData } from '../../classes/user-data';
 import { HttpClient } from '@angular/common/http';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../page-game/buy/prezzi/services/cart.service';
 
 @Component({
   selector: 'app-carello',
@@ -14,23 +15,24 @@ export class CarelloComponent {
   private authService: AuthService = inject(AuthService);
   currentUser: WritableSignal<UserData | null> = signal<UserData | null>(null);
   private router = inject(Router);
+  cartService = inject(CartService);
+  cartItems: any[] = [];
 
-
-    constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
+    // Sottoscrizione al carrello (sempre aggiornata)
+    this.cartService.cartItems$.subscribe((items: any[]) => {
+      this.cartItems = items;
+    });
+    // Effetto solo per utente
     effect(() => {
-      const user = this.authService.currentUser();  // questo dovrebbe restituire i dati anche dopo un nuovo login
+      const user = this.authService.currentUser();
       if (user) {
-        console.log('Utente loggato:', user);
         this.currentUser.set(user);
       } else {
-        console.log('Utente non loggato');
-        this.currentUser.set(null); // importante anche per l'effetto reactive
-        this.router.navigate(['/login']); // redirect se non loggato
-
+        this.currentUser.set(null);
       }
     });
   }
-
 
   aggiornaDenaro(nuovoImporto: number) {
     if (this.currentUser() !== null) {
@@ -51,8 +53,4 @@ export class CarelloComponent {
       this.authService.saveUserData(this.currentUser()!);
     }
   }
-
-  
-  
-
 }
